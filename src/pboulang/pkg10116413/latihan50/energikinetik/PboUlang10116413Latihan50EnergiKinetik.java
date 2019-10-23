5b20d0d6ac8ff3f723ecb39963d91b1410a20971
Harry Apriadi <harryapriadi1@gmail.com>
2019-10-23 15:58:33 +0700

    PboUlang-10116413-Latihan50-EnergiKinetik

---
+++ src/pboulang/pkg10116413/latihan50/energikinetik/PboUlang10116413Latihan50EnergiKinetik.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboulang.pkg10116413.latihan50.energikinetik;
+
+/**
+ *
+ * @author Lenovo
+ * NAMA         : HARRY APRIADI
+ * KELAS        : PBOULANG
+ * NIM          : 10116413
+ * DESKRIPSI    : program ini berisi program menghitung energi kinerik dalam
+ *                keadaan bergerak dan keadaan diam
+ */
+public class PboUlang10116413Latihan50EnergiKinetik {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    Energi objE = new Energi();
+        /*rubah gram ke kilogram dengan cara 145 gram dibgai dengan 1000
+        menghasilkan nilai 0.145 kilogram*/
+        objE.setMassa(0.145);
+        objE.setKecepatan(25);
+        System.out.println("Sebuah bola baseball dengan massa 145 g dilempar "
+                + "dengan kecepatan 25 m/s");
+        System.out.println("a. Berapakah Energi Kinetiknya ?");
+        System.out.println("b. Berapakah usaha yang dilakukan pada bola untuk "
+                + "mencapai kecepatan ini jika dimulai dari keadaan diam ?");
+        System.out.println("");
+        System.out.println("=====JAWABAN=====");
+        System.out.println("a. Energi Kinetiknya Adalah "+objE.hitungEnergiKinetik()+" Joule");
+        System.out.println("b. Usahanya adalah "+objE.hitungUsaha()+" Joule");
+        System.out.println("");
+        System.out.println("Jadi energi kinetik dan usahanya bernilai sama yaitu "
+                + "sebesar 45.3125 Joule");
+    }
+    
+}
